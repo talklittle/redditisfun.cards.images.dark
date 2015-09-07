bb4a991a9515a3cd8e7d7945594827737a173696
Andrew Shu <talklittle@gmail.com>
2015-09-06 21:37:43 -0700

    thumbnails: parse URL to get path
    fixes URLs with querystring
    
    uses neturl.lua
    https://github.com/golgote/neturl

--- thumbnails.lua
+++ thumbnails.lua
@@ -1,35 +1,22 @@
 local thumbnails = {}
 
+local neturl = require "neturl"
+
 function thumbnails.getImageUrl(url)
-    local urlLower = url:lower()
+    local u = neturl.parse(url)
 
-    local scheme
-    if urlLower:sub(1, 5) == "https" then
-        scheme = "https"
-    else
-        scheme = "http"
-    end
-
-    local withoutScheme = urlLower:sub(scheme:len() + 4)
-    local pathSlashIndex = withoutScheme:find("/")
-
-    local host
-    if pathSlashIndex ~= nil then
-        host = withoutScheme:sub(1, pathSlashIndex - 1)
-    else
-        host = withoutScheme:sub(1)
-    end
+    local pathLower = u.path:lower()
 
     -- get common extensions (.jpg, .png, .gif)
-    local last4 = urlLower:sub(-4)
+    local last4 = pathLower:sub(-4)
     -- 4-char extensions (.jpeg, .gifv)
-    local last5 = urlLower:sub(-5)
+    local last5 = pathLower:sub(-5)
 
-    if withoutScheme:sub(1, 12) == "imgur.com/a/" then
+    if u.host:sub(1, 12) == "imgur.com/a/" then
         return url
-    elseif withoutScheme:sub(1, 18) == "imgur.com/gallery/" then
+    elseif u.host:sub(1, 18) == "imgur.com/gallery/" then
         return nil
-    elseif host == "i.imgur.com" or host == "imgur.com" then
+    elseif u.host == "i.imgur.com" or u.host == "imgur.com" then
         if last4 == ".jpg" or last4 == ".gif" or last4 == ".png" then
             return url:sub(1, -5) .. "l.jpg"
         elseif last5 == ".jpeg" or last5 == ".gifv" then
